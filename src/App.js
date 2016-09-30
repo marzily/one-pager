dcd566c6df6bf07c89a7983816bbe4f58bfff2ae
margie <ly.margarett@gmail.com>
2016-09-30 13:02:42 -0700

    complete exercise, remove state

--- src/App.js
+++ src/App.js
@@ -1,15 +1,48 @@
 import React, { Component } from 'react';
-import validator from 'validator';
 import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
-  handleSubmit(event) {
-
-  }
-  isEmail(value) {
+  validateEmail(value) {
     var re = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
     return re.test(value);
+  }
+
+  validateFields(firstname, email) {
+    if (!firstname && !email) {
+      var missing = "First Name and Email are required.";
+    } else if (!firstname) {
+      var missing = "First Name is required.";
+    } else if (!email) {
+      var missing = "Email is required.";
+    }
+
+    return missing;
+  }
+
+  handleSubmit(event) {
+    event.preventDefault();
+    var form = document.getElementById('signup');
+
+    var firstname = form.firstname.value.trim()
+    var lastname = form.lastname.value.trim()
+    var email = form.email.value.trim()
+
+    var fieldsMissing = this.validateFields(firstname, email);
+    var emailValid = this.validateEmail(email);
+    this.appendError(fieldsMissing, emailValid);
+  }
+
+  appendError(fieldsMissing, emailValid) {
+    if (fieldsMissing){
+      var missingMsg = document.createTextNode(fieldsMissing);
+      document.getElementById('signup').appendChild(missingMsg);
+    }
+
+    if (!emailValid) {
+      var validMsg = document.createTextNode("Email is invalid.");
+      document.getElementById('signup').appendChild(validMsg);
+    }
   }
 
   render() {
@@ -21,13 +54,13 @@ class App extends Component {
         </div>
         <br/>
 
-        <form onSubmit={this.handleSubmit}>
+        <form id="signup" onSubmit={this.handleSubmit.bind(this)}>
           First Name:<br/>
           <input type="text" name="firstname"/><br/><br/>
           Last Name:<br/>
           <input type="text" name="lastname"/><br/><br/>
           Email:<br/>
-          <input type="text" name="email" validations="isEmail" validationError="This is not a valid email"/><br/><br/>
+          <input type="text" name="email"/><br/><br/>
           <input type="submit" name="Submit"/><br/><br/>
         </form>
 
